f45208b2d553f56b6c965d538519eb7c87ffb7c4
Jake Skelcy <jskelcy@uber.com>
2018-03-22 09:47:38 -0400

    remove RuleSetsChanges struct

--- rules/models/changes/ruleset.go
+++ rules/models/changes/ruleset.go
@@ -16,22 +16,3 @@ func (d *RuleSetChanges) Sort() {
 	sort.Sort(mappingRuleChangesByOpAscNameAscIDAsc(d.MappingRuleChanges))
 	sort.Sort(rollupRuleChangesByOpAscNameAscIDAsc(d.RollupRuleChanges))
 }
-
-// RuleSetsChanges is a list of ruleset diffs.
-type RuleSetsChanges []RuleSetChanges
-
-// Sort sorts the ruleset diffs by op and namespace in ascending order.
-func (d RuleSetsChanges) Sort() {
-	for i := range d {
-		d[i].Sort()
-	}
-	sort.Sort(ruleSetChangesNamespaceAsc(d))
-}
-
-type ruleSetChangesNamespaceAsc []RuleSetChanges
-
-func (a ruleSetChangesNamespaceAsc) Len() int      { return len(a) }
-func (a ruleSetChangesNamespaceAsc) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ruleSetChangesNamespaceAsc) Less(i, j int) bool {
-	return a[i].Namespace < a[j].Namespace
-}
--- rules/models/changes/ruleset_test.go
+++ rules/models/changes/ruleset_test.go
@@ -9,171 +9,155 @@ import (
 )
 
 func TestRuleSetChangeSetsSort(t *testing.T) {
-	expected := RuleSetsChanges{
-		RuleSetChanges{
-			Namespace: "service1",
-			MappingRuleChanges: []MappingRuleChange{
-				{
-					Op: AddOp,
-					RuleData: &models.MappingRule{
-						Name: "Add1",
-					},
-				},
-				{
-					Op: AddOp,
-					RuleData: &models.MappingRule{
-						Name: "Add2",
-					},
-				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("mrID2"),
-					RuleData: &models.MappingRule{
-						Name: "change1",
-					},
-				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("mrID3"),
-					RuleData: &models.MappingRule{
-						Name: "change2",
-					},
-				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("mrID1"),
-					RuleData: &models.MappingRule{
-						Name: "change3",
-					},
+	expected := RuleSetChanges{
+		Namespace: "service1",
+		MappingRuleChanges: []MappingRuleChange{
+			{
+				Op: AddOp,
+				RuleData: &models.MappingRule{
+					Name: "Add1",
 				},
 			},
-			RollupRuleChanges: []RollupRuleChange{
-				{
-					Op: AddOp,
-					RuleData: &models.RollupRule{
-						Name: "Add1",
-					},
+			{
+				Op: AddOp,
+				RuleData: &models.MappingRule{
+					Name: "Add2",
 				},
-				{
-					Op: AddOp,
-					RuleData: &models.RollupRule{
-						Name: "Add2",
-					},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("mrID2"),
+				RuleData: &models.MappingRule{
+					Name: "change1",
 				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("rrID2"),
-					RuleData: &models.RollupRule{
-						Name: "change1",
-					},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("mrID3"),
+				RuleData: &models.MappingRule{
+					Name: "change2",
 				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("rrID3"),
-					RuleData: &models.RollupRule{
-						Name: "change2",
-					},
-				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("rrID1"),
-					RuleData: &models.RollupRule{
-						Name: "change3",
-					},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("mrID1"),
+				RuleData: &models.MappingRule{
+					Name: "change3",
 				},
 			},
 		},
-		RuleSetChanges{
-			Namespace: "service2",
-		},
-		RuleSetChanges{
-			Namespace: "service3",
+		RollupRuleChanges: []RollupRuleChange{
+			{
+				Op: AddOp,
+				RuleData: &models.RollupRule{
+					Name: "Add1",
+				},
+			},
+			{
+				Op: AddOp,
+				RuleData: &models.RollupRule{
+					Name: "Add2",
+				},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("rrID2"),
+				RuleData: &models.RollupRule{
+					Name: "change1",
+				},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("rrID3"),
+				RuleData: &models.RollupRule{
+					Name: "change2",
+				},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("rrID1"),
+				RuleData: &models.RollupRule{
+					Name: "change3",
+				},
+			},
 		},
 	}
 
-	ruleSets.Sort()
-	require.Equal(t, expected[0].RollupRuleChanges, ruleSets[0].RollupRuleChanges)
-	require.Equal(t, expected[0].MappingRuleChanges, ruleSets[0].MappingRuleChanges)
+	ruleSet.Sort()
+	require.Equal(t, expected, ruleSet)
+	require.Equal(t, expected, ruleSet)
 }
 
 var (
-	ruleSets = RuleSetsChanges{
-		RuleSetChanges{
-			Namespace: "service3",
-		},
-		RuleSetChanges{
-			Namespace: "service2",
-		},
-		RuleSetChanges{
-			Namespace: "service1",
-			MappingRuleChanges: []MappingRuleChange{
-				{
-					Op:     ChangeOp,
-					RuleID: p("mrID1"),
-					RuleData: &models.MappingRule{
-						Name: "change3",
-					},
-				},
-				{
-					Op: AddOp,
-					RuleData: &models.MappingRule{
-						Name: "Add2",
-					},
-				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("mrID2"),
-					RuleData: &models.MappingRule{
-						Name: "change1",
-					},
-				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("mrID3"),
-					RuleData: &models.MappingRule{
-						Name: "change2",
-					},
-				},
-				{
-					Op: AddOp,
-					RuleData: &models.MappingRule{
-						Name: "Add1",
-					},
+	ruleSet = RuleSetChanges{
+		Namespace: "service1",
+		MappingRuleChanges: []MappingRuleChange{
+			{
+				Op:     ChangeOp,
+				RuleID: p("mrID1"),
+				RuleData: &models.MappingRule{
+					Name: "change3",
 				},
 			},
-			RollupRuleChanges: []RollupRuleChange{
-				{
-					Op:     ChangeOp,
-					RuleID: p("rrID1"),
-					RuleData: &models.RollupRule{
-						Name: "change3",
-					},
+			{
+				Op: AddOp,
+				RuleData: &models.MappingRule{
+					Name: "Add2",
 				},
-				{
-					Op: AddOp,
-					RuleData: &models.RollupRule{
-						Name: "Add2",
-					},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("mrID2"),
+				RuleData: &models.MappingRule{
+					Name: "change1",
 				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("rrID2"),
-					RuleData: &models.RollupRule{
-						Name: "change1",
-					},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("mrID3"),
+				RuleData: &models.MappingRule{
+					Name: "change2",
 				},
-				{
-					Op:     ChangeOp,
-					RuleID: p("rrID3"),
-					RuleData: &models.RollupRule{
-						Name: "change2",
-					},
+			},
+			{
+				Op: AddOp,
+				RuleData: &models.MappingRule{
+					Name: "Add1",
 				},
-				{
-					Op: AddOp,
-					RuleData: &models.RollupRule{
-						Name: "Add1",
-					},
+			},
+		},
+		RollupRuleChanges: []RollupRuleChange{
+			{
+				Op:     ChangeOp,
+				RuleID: p("rrID1"),
+				RuleData: &models.RollupRule{
+					Name: "change3",
+				},
+			},
+			{
+				Op: AddOp,
+				RuleData: &models.RollupRule{
+					Name: "Add2",
+				},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("rrID2"),
+				RuleData: &models.RollupRule{
+					Name: "change1",
+				},
+			},
+			{
+				Op:     ChangeOp,
+				RuleID: p("rrID3"),
+				RuleData: &models.RollupRule{
+					Name: "change2",
+				},
+			},
+			{
+				Op: AddOp,
+				RuleData: &models.RollupRule{
+					Name: "Add1",
 				},
 			},
 		},
