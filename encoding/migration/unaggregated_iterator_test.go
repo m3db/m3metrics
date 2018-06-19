6a778d29c533085bd9c2bd92047fa5b9267bc3c6
Xi Chen <xichen@uber.com>
2018-06-19 14:01:56 -0400

    Closing migration iterator should close inner iterators

--- encoding/migration/unaggregated_iterator_test.go
+++ encoding/migration/unaggregated_iterator_test.go
@@ -818,3 +818,25 @@ func TestUnaggregatedIteratorNextOnClose(t *testing.T) {
 	// Verify that closing a second time is a no op.
 	it.Close()
 }
+
+func TestUnaggregatedIteratorClose(t *testing.T) {
+	it := NewUnaggregatedIterator(
+		nil,
+		msgpack.NewUnaggregatedIteratorOptions(),
+		protobuf.NewUnaggregatedOptions(),
+	)
+	require.False(t, it.(*unaggregatedIterator).closed)
+	require.NotNil(t, it.(*unaggregatedIterator).msgpackIt)
+	require.NotNil(t, it.(*unaggregatedIterator).protobufIt)
+
+	it.Close()
+	require.True(t, it.(*unaggregatedIterator).closed)
+	require.Nil(t, it.(*unaggregatedIterator).msgpackIt)
+	require.Nil(t, it.(*unaggregatedIterator).protobufIt)
+
+	// Verify that closing a second time is a no op.
+	it.Close()
+	require.True(t, it.(*unaggregatedIterator).closed)
+	require.Nil(t, it.(*unaggregatedIterator).msgpackIt)
+	require.Nil(t, it.(*unaggregatedIterator).protobufIt)
+}
